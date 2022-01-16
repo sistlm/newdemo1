4cbc622d6c159d6e54dc16e71a19820510a58c54
Sistla Durga Mahender Rao <sd.mahenderrao@gmail.com>
2022-01-16 17:50:44 +0530

    add this code

---
+++ newgroup1/newgroup1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace newgroup1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
